6b2ccfb737d005403b6a308e260f48abf885415a
Martin Kuba <makub@ics.muni.cz>
2017-11-01 18:25:58 +0100

    replaced PostgreSQL with MySQL, added logging for token release

--- oidc-idp/src/main/java/cz/muni/ics/oidc/PerunAuthenticationSuccessHandler.java
+++ oidc-idp/src/main/java/cz/muni/ics/oidc/PerunAuthenticationSuccessHandler.java
@@ -1,7 +1,12 @@
 package cz.muni.ics.oidc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -18,9 +23,13 @@ import static org.mitre.openid.connect.web.AuthenticationTimeStamper.AUTH_TIMEST
  */
 public class PerunAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+	private final static Logger log = LoggerFactory.getLogger(PerunAuthenticationSuccessHandler.class);
+
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 		Date authTimestamp = new Date();
 		request.getSession().setAttribute(AUTH_TIMESTAMP, authTimestamp);
+
+
 	}
 }
---
+++ oidc-idp/src/main/java/cz/muni/ics/oidc/PerunTokenEnhancer.java
@@ -0,0 +1,29 @@
+package cz.muni.ics.oidc;
+
+import org.mitre.openid.connect.token.ConnectTokenEnhancer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author Martin Kuba makub@ics.muni.cz
+ */
+public class PerunTokenEnhancer extends ConnectTokenEnhancer {
+
+	private final static Logger log = LoggerFactory.getLogger(PerunTokenEnhancer.class);
+
+	@Override
+	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+		log.info("enhance(accessToken={},authentication={})", accessToken, authentication);
+		OAuth2Request oAuth2Request = authentication.getOAuth2Request();
+		log.info("oAuth2Request.grantType: {}", oAuth2Request.getGrantType());
+		log.info("oAuth2Request.clientId: {}", oAuth2Request.getClientId());
+		log.info("oAuth2Request.redirectUri: {}", oAuth2Request.getRedirectUri());
+		return super.enhance(accessToken, authentication);
+	}
+
+}
