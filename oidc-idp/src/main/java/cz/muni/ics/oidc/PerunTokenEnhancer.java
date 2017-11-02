890c52ea03d48fce55ea68b431666d2be591b0aa
Martin Kuba <makub@ics.muni.cz>
2017-11-02 14:13:49 +0100

    even better logging of issued tokens

--- oidc-idp/src/main/java/cz/muni/ics/oidc/PerunTokenEnhancer.java
+++ oidc-idp/src/main/java/cz/muni/ics/oidc/PerunTokenEnhancer.java
@@ -3,12 +3,13 @@ package cz.muni.ics.oidc;
 import org.mitre.openid.connect.token.ConnectTokenEnhancer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Request;
 
 /**
- * Created with IntelliJ IDEA.
+ * Just logs issued tokens.
  *
  * @author Martin Kuba makub@ics.muni.cz
  */
@@ -18,12 +19,12 @@ public class PerunTokenEnhancer extends ConnectTokenEnhancer {
 
 	@Override
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-		log.info("enhance(accessToken={},authentication={})", accessToken, authentication);
-		log.info("principal: {}",authentication.getPrincipal());
+		log.trace("enhance(accessToken={},authentication={})", accessToken, authentication);
+		Object principal = authentication.getPrincipal();
+		String userId = principal instanceof User ? ((User) principal).getUsername() : principal.toString();
 		OAuth2Request oAuth2Request = authentication.getOAuth2Request();
-		log.info("grantType: {}", oAuth2Request.getGrantType());
-		log.info("clientId: {}", oAuth2Request.getClientId());
-		log.info("redirectUri: {}", oAuth2Request.getRedirectUri());
+		log.info("userId: {}, clientId: {}, grantType: {}, redirectUri: {}",
+				userId, oAuth2Request.getClientId(), oAuth2Request.getGrantType(), oAuth2Request.getRedirectUri());
 		return super.enhance(accessToken, authentication);
 	}
 
