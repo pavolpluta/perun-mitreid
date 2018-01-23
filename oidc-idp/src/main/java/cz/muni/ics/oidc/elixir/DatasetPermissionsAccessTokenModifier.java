e81f92bc111c674e8cfccb2839610b28eebbcb7e
Martin Kuba <makub@ics.muni.cz>
2018-01-23 15:17:13 +0000

    rough implementaion of EGA dataset permissions

--- oidc-idp/src/main/java/cz/muni/ics/oidc/elixir/DatasetPermissionsAccessTokenModifier.java
+++ oidc-idp/src/main/java/cz/muni/ics/oidc/elixir/DatasetPermissionsAccessTokenModifier.java
@@ -1,15 +1,27 @@
 package cz.muni.ics.oidc.elixir;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jwt.JWTClaimsSet;
+import cz.muni.ics.oidc.PerunAuthenticationFilter;
 import cz.muni.ics.oidc.PerunTokenEnhancer;
 import net.minidev.json.JSONArray;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.InterceptingClientHttpRequestFactory;
+import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,21 +29,58 @@ import java.util.Map;
  * @author Martin Kuba makub@ics.muni.cz
  */
 public class DatasetPermissionsAccessTokenModifier implements PerunTokenEnhancer.AccessTokenClaimsModifier{
+
+    private final static Logger log = LoggerFactory.getLogger(DatasetPermissionsAccessTokenModifier.class);
+
     @Override
     public void modifyClaims(JWTClaimsSet.Builder builder, OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-        if(accessToken.getScope().contains("datasets_permissions")) {
+        if(accessToken.getScope().contains("permissions_ega")) {
             //get permissions data
-            //TODO read real permissions data
-            JSONArray perms = new JSONArray();
-            Map<String, Object> p1 = new LinkedHashMap<>();
-            perms.add(p1);
-            p1.put("affiliation", null);
-            List<String> ds = new ArrayList<>();
-            p1.put("datasets", ds);
-            ds.add("https://www.ebi.ac.uk/ega/datasets/EGAD00000000053");
-            ds.add("https://www.ebi.ac.uk/ega/datasets/EGAD00000000054");
 
-            builder.claim("permissions", perms);
+            try {
+                JsonNode jsonNode = makeRestCall("?affiliation=member@csc.fi");
+                String rawJson = new ObjectMapper().writeValueAsString(jsonNode.path("permissions"));
+                log.debug("EGA response: {}",rawJson);
+                //add data
+                JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
+                Object perms = parser.parse(rawJson);
+                builder.claim("permissions_ega", perms);
+            } catch (ParseException | JsonProcessingException e) {
+                log.error("cannot parse Permissions API response",e);
+            }
+
+        }
+    }
+
+    private String egaUrl = "http://193.167.189.108:8080/user/jack/";
+    private String egaUser = "user";
+    private String egaPassword = "password";
+
+    private JsonNode makeRestCall(String urlPart) {
+        //prepare basic auth
+        RestTemplate restTemplate = new RestTemplate();
+//        List<ClientHttpRequestInterceptor> interceptors =
+//                Collections.singletonList(new BasicAuthorizationInterceptor(egaUser, egaPassword));
+//        restTemplate.setRequestFactory(new InterceptingClientHttpRequestFactory(restTemplate.getRequestFactory(), interceptors));
+        String actionUrl = egaUrl + urlPart;
+        //make the call
+        try {
+            log.trace("calling {}",actionUrl);
+            return restTemplate.getForObject(actionUrl, JsonNode.class);
+        } catch (HttpClientErrorException ex) {
+            MediaType contentType = ex.getResponseHeaders().getContentType();
+            String body = ex.getResponseBodyAsString();
+            log.error("HTTP ERROR " + ex.getRawStatusCode() + " URL " + actionUrl + " Content-Type: " + contentType);
+            if ("json".equals(contentType.getSubtype())) {
+                try {
+                    log.error(new ObjectMapper().readValue(body, JsonNode.class).path("message").asText());
+                } catch (IOException e) {
+                    log.error("cannot parse error message from JSON",e);
+                }
+            } else {
+                log.error(ex.getMessage());
+            }
+            throw new RuntimeException("cannot make REST call",ex);
         }
     }
 }
