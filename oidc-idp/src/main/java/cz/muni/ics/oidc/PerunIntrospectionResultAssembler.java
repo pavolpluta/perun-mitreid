0408c40c9e6b72ffec697a060a096c42b66d425f
Martin Kuba <makub@ics.muni.cz>
2018-06-06 09:33:31 +0200

    implemented releasing user claim from introspection endpoint

--- oidc-idp/src/main/java/cz/muni/ics/oidc/PerunIntrospectionResultAssembler.java
+++ oidc-idp/src/main/java/cz/muni/ics/oidc/PerunIntrospectionResultAssembler.java
@@ -1,10 +1,12 @@
 package cz.muni.ics.oidc;
 
+import com.google.gson.JsonElement;
 import org.mitre.oauth2.model.OAuth2AccessTokenEntity;
 import org.mitre.oauth2.model.OAuth2RefreshTokenEntity;
 import org.mitre.oauth2.service.impl.DefaultIntrospectionResultAssembler;
 import org.mitre.openid.connect.config.ConfigurationPropertiesBean;
 import org.mitre.openid.connect.model.UserInfo;
+import org.mitre.openid.connect.service.ScopeClaimTranslationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,28 +22,41 @@ public class PerunIntrospectionResultAssembler extends DefaultIntrospectionResul
 
 	private final static Logger log = LoggerFactory.getLogger(PerunIntrospectionResultAssembler.class);
 
+	public PerunIntrospectionResultAssembler(ConfigurationPropertiesBean configBean, ScopeClaimTranslationService translator) {
+		this.configBean = configBean;
+		this.translator = translator;
+	}
+
 	private ConfigurationPropertiesBean configBean;
+	private ScopeClaimTranslationService translator;
 
 	@Override
 	public Map<String, Object> assembleFrom(OAuth2AccessTokenEntity accessToken, UserInfo userInfo, Set<String> authScopes) {
+		log.info("adding claims at Introspection endpoint for client {}({}) and user {}({})",
+				accessToken.getClient().getClientId(), accessToken.getClient().getClientName(), userInfo.getSub(), userInfo.getName());
 		Map<String, Object> map = super.assembleFrom(accessToken, userInfo, authScopes);
-		addDataToResponse(map);
+		addDataToResponse(map, userInfo, authScopes);
 		return map;
 	}
 
 	@Override
 	public Map<String, Object> assembleFrom(OAuth2RefreshTokenEntity refreshToken, UserInfo userInfo, Set<String> authScopes) {
 		Map<String, Object> map = super.assembleFrom(refreshToken, userInfo, authScopes);
-		addDataToResponse(map);
+		addDataToResponse(map, userInfo, authScopes);
 		return map;
 	}
 
-	private void addDataToResponse(Map<String, Object> map) {
+	private void addDataToResponse(Map<String, Object> map, UserInfo userInfo, Set<String> authScopes) {
 		log.debug("adding iss to introspection response {}", map);
 		map.put("iss", configBean.getIssuer());
+		log.debug("adding user claims");
+		Set<String> authorizedClaims = translator.getClaimsForScopeSet(authScopes);
+		for (Map.Entry<String, JsonElement> claim : userInfo.toJson().entrySet()) {
+			if (authorizedClaims.contains(claim.getKey()) && claim.getValue() != null && !claim.getValue().isJsonNull()) {
+				log.debug("adding claim {} with value {}", claim.getKey(), claim.getValue());
+				map.put(claim.getKey(), claim.getValue());
+			}
+		}
 	}
 
-	public void setConfigBean(ConfigurationPropertiesBean configBean) {
-		this.configBean = configBean;
-	}
 }
